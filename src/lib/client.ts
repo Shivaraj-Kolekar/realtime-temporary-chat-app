2f99e3c2780d1500fb0e882a1aa753287300165f
Shivaraj-Kolekar <shivkolekar01@gmail.com>
2026-01-08 14:57:55 +0530

    fixed deployement issue

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,4 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "@/app/api/[[...slugs]]/route";
 
-export const api = treaty<App>(process.env.NEXT_PUBLIC_API_URL!);
+export const client = treaty<App>(process.env.NEXT_PUBLIC_API_URL!);
